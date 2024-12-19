23b5f5a0f25375ab86b02740ff63ded912e45af1
Gustavo <gustavohs2004@gmail.com>
2024-12-19 18:16:45 -0300

    Implementando SignalR em mapa para ver usuários

---
+++ Snarf.Back/Snarf.API/Controllers/LocationHub.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Serilog;
+using System.Collections.Concurrent;
+
+public class LocationHub : Hub
+{
+    // Dicionário para armazenar a localização dos usuários
+    private static readonly ConcurrentDictionary<string, (double latitude, double longitude)> _userLocations = new ConcurrentDictionary<string, (double, double)>();
+
+    // Método chamado quando o cliente envia sua localização
+    public async Task UpdateLocation(double latitude, double longitude)
+    {
+        Log.Warning($"{Context.ConnectionId} conectou");
+
+        // Atualiza a localização do usuário conectado
+        _userLocations[Context.ConnectionId] = (latitude, longitude);
+
+        // Envia a localização para todos os outros clientes conectados
+        await Clients.Others.SendAsync("ReceiveLocation", latitude, longitude);
+    }
+
+    // Método chamado quando um novo cliente se conecta
+    public override async Task OnConnectedAsync()
+    {
+        Log.Warning($"{Context.ConnectionId} conectou");
+
+        // Envia as localizações de todos os usuários para o novo cliente
+        foreach (var userLocation in _userLocations)
+        {
+            await Clients.Caller.SendAsync("ReceiveLocation", userLocation.Value.latitude, userLocation.Value.longitude);
+        }
+
+        await base.OnConnectedAsync();
+    }
+
+    // Método chamado quando um cliente se desconecta
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var connectionId = Context.ConnectionId;
+        Log.Warning($"{connectionId} desconectou");
+        // Notifica todos os clientes sobre a desconexão do usuário
+        await Clients.All.SendAsync("UserDisconnected", connectionId);
+
+        // Remove a localização do usuário desconectado
+        _userLocations.TryRemove(connectionId, out _);
+
+        await base.OnDisconnectedAsync(exception);
+    }
+
+}
--- snarf/lib/pages/home_page.dart
+++ snarf/lib/pages/home_page.dart
@@ -3,7 +3,10 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:location/location.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
+import 'package:signalr_netcore/hub_connection.dart';
+import 'package:signalr_netcore/hub_connection_builder.dart';
 import 'package:snarf/providers/theme_provider.dart';
+import 'package:snarf/utils/api_constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,12 +20,15 @@ class _HomePageState extends State<HomePage> {
   bool _isLocationLoaded = false;
   late MapController _mapController;
   late Marker _userLocationMarker;
+  List<Marker> _otherUserMarkers = [];
+  late HubConnection _hubConnection;
 
   @override
   void initState() {
     super.initState();
     _mapController = MapController();
     _getCurrentLocation();
+    _setupSignalR();
   }
 
   Future<void> _getCurrentLocation() async {
@@ -55,6 +61,47 @@ class _HomePageState extends State<HomePage> {
         ),
       );
     });
+
+    _sendLocationUpdate();
+  }
+
+  void _setupSignalR() {
+    // Criação da conexão SignalR com o Hub
+    _hubConnection = HubConnectionBuilder()
+        .withUrl(
+            '${ApiConstants.baseUrl.replaceAll('/api', '')}/locationHub') // URL do seu SignalR Hub
+        .build();
+
+    // Ouve por atualizações de localização de outros usuários
+    _hubConnection.on("ReceiveLocation", (args) {
+      final latitude = args?[0] as double;
+      final longitude = args?[1] as double;
+      setState(() {
+        // Adiciona a localização recebida à lista de marcadores de outros usuários
+        _otherUserMarkers.add(Marker(
+          point: LatLng(latitude, longitude),
+          width: 30,
+          height: 30,
+          child: const Icon(
+            Icons.location_on,
+            color: Colors.blue,
+            size: 30,
+          ),
+        ));
+      });
+    });
+
+    _hubConnection.start()?.catchError((err) {
+      print("Erro ao iniciar a conexão SignalR: $err");
+    });
+  }
+
+  void _sendLocationUpdate() async {
+    // Envia a localização atualizada para o Hub SignalR
+    if (_hubConnection.state == HubConnectionState.Connected) {
+      await _hubConnection.invoke("UpdateLocation",
+          args: [_currentLocation.latitude!, _currentLocation.longitude!]);
+    }
   }
 
   void _recentralizeMap() {
@@ -92,6 +139,12 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void dispose() {
+    _hubConnection.stop();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -126,6 +179,8 @@ class _HomePageState extends State<HomePage> {
                       MarkerLayer(
                         markers: [
                           _userLocationMarker,
+                          ..._otherUserMarkers,
+                          // Adiciona os marcadores dos outros usuários
                         ],
                       ),
                     ],
