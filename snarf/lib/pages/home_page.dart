b81d0feae55c84fc33d0ea019abfb96cab1df42a
Gustavo <gustavohs2004@gmail.com>
2024-12-18 22:36:10 -0300

    Adicionando mapa

--- snarf/lib/pages/home_page.dart
+++ snarf/lib/pages/home_page.dart
@@ -1,15 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:location/location.dart';
+import 'package:latlong2/latlong.dart';
 import '../services/api_service.dart';
 import '../pages/login_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late LocationData _currentLocation;
+  bool _isLocationLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getCurrentLocation();
+  }
+
+  Future<void> _getCurrentLocation() async {
+    Location location = Location();
+    bool serviceEnabled = await location.serviceEnabled();
+    if (!serviceEnabled) {
+      serviceEnabled = await location.requestService();
+      if (!serviceEnabled) {
+        return;
+      }
+    }
+
+    PermissionStatus permissionGranted = await location.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await location.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _currentLocation = await location.getLocation();
+    setState(() {
+      _isLocationLoaded = true;
+    });
+  }
 
   Future<void> _logout(BuildContext context) async {
     await ApiService.logout();
     Navigator.pushReplacement(
       context,
       MaterialPageRoute(builder: (context) => const LoginPage()),
+    );
+  }
+
+  void _showTokenBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return FutureBuilder<String?>(
+          future: ApiService.getToken(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasData && snapshot.data != null) {
+              return ListTile(
+                title: Text('Token: ${snapshot.data}'),
+              );
+            } else {
+              return const ListTile(
+                title: Text('Nenhum token encontrado.'),
+              );
+            }
+          },
+        );
+      },
     );
   }
 
@@ -26,18 +91,38 @@ class HomePage extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: FutureBuilder<String?>(
-          future: ApiService.getToken(),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasData && snapshot.data != null) {
-              return Text('Token: ${snapshot.data}');
-            } else {
-              return const Text('Nenhum token encontrado.');
-            }
-          },
-        ),
+        child: _isLocationLoaded
+            ? Stack(
+                children: [
+                  FlutterMap(
+                    options: MapOptions(
+                      initialCenter: LatLng(_currentLocation.latitude!,
+                          _currentLocation.longitude!),
+                      initialZoom: 15.0,
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate:
+                            'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: ElevatedButton(
+                        onPressed: () => _showTokenBottomSheet(context),
+                        child: const Text('Exibir Token'),
+                      ),
+                    ),
+                  ),
+                ],
+              )
+            : const CircularProgressIndicator(),
       ),
     );
   }
