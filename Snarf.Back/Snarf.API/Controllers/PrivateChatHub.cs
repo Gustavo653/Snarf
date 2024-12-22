30a16c3d217c1f6727a0295ee4ce84a402f7bedd
Gustavo <gustavohs2004@gmail.com>
2024-12-22 08:31:44 -0300

    Implementando listagem de chat privado

--- Snarf.Back/Snarf.API/Controllers/PrivateChatHub.cs
+++ Snarf.Back/Snarf.API/Controllers/PrivateChatHub.cs
@@ -25,6 +25,60 @@ namespace Snarf.API.Controllers
             await base.OnConnectedAsync();
         }
 
+        public async Task GetRecentChats()
+        {
+            var userId = GetUserId();
+
+            Log.Information($"Usuário {userId} solicitou a lista de conversas recentes.");
+
+            // Carrega todas as mensagens relevantes do banco
+            var messages = await _chatMessageRepository.GetEntities()
+                .Where(m => m.Sender.Id == userId || m.Receiver.Id == userId)
+                .Select(m => new
+                {
+                    SenderId = m.Sender.Id,
+                    SenderName = m.Sender.Name,
+                    ReceiverId = m.Receiver.Id,
+                    ReceiverName = m.Receiver.Name,
+                    m.Message,
+                    m.CreatedAt
+                })
+                .ToListAsync();
+
+            // Processa os dados em memória
+            var recentChats = messages
+                .GroupBy(m =>
+                {
+                    // Usa uma tupla para comparar os guids de maneira consistente e evitar duplicação de nomes
+                    var senderId = m.SenderId;
+                    var receiverId = m.ReceiverId;
+
+                    // Ordena os guids para garantir que a chave seja única, independentemente da ordem de envio/recebimento
+                    return senderId.CompareTo(receiverId) < 0
+                        ? (senderId, receiverId)
+                        : (receiverId, senderId);
+                })
+                .Select(group => new
+                {
+                    // A chave da tupla agora é usada para obter os ids de sender e receiver
+                    UserId = group.Key.Item1, // Primeiro id da tupla (o menor)
+                    UserName = group.Key.Item1 == group.First().SenderId
+                        ? group.First().ReceiverName
+                        : group.First().SenderName, // Nome do outro usuário
+                    LastMessage = group.OrderByDescending(m => m.CreatedAt).FirstOrDefault()?.Message,
+                    LastMessageDate = group.Max(m => m.CreatedAt)
+                })
+                .OrderByDescending(c => c.LastMessageDate) // Ordena pela data da última mensagem
+                .ToList();
+
+            var messagesJson = JsonSerializer.Serialize(recentChats, options: new JsonSerializerOptions { Converters = { new DateTimeConverterToTimeZone("America/Sao_Paulo") } });
+
+            Log.Information($"Usuário {userId} recebeu {recentChats.Count} conversas recentes.");
+
+            // Enviar as conversas recentes para o cliente
+            await Clients.Caller.SendAsync("ReceiveRecentChats", messagesJson);
+        }
+
         public async Task GetPreviousMessages(string receiverUserId)
         {
             var userId = GetUserId();
@@ -65,10 +119,13 @@ namespace Snarf.API.Controllers
         public async Task SendPrivateMessage(string receiverUserId, string message)
         {
             var senderUserId = GetUserId();
+            var senderUserName = Context.User?.Identity?.Name ?? "Desconhecido";  // Obter o nome do usuário a partir do contexto
 
+            // Enfileira a persistência da mensagem em segundo plano
             Task.Run(() => _backgroundJobClient.Enqueue(() => _messagePersistenceService.PersistMessageAsync(senderUserId, receiverUserId, message, DateTime.UtcNow)));
 
-            await Clients.User(receiverUserId).SendAsync("ReceivePrivateMessage", message);
+            // Envia a mensagem para o receptor, incluindo os detalhes do remetente
+            await Clients.User(receiverUserId).SendAsync("ReceivePrivateMessage", senderUserId, senderUserName, message);
         }
     }
 }
--- snarf/lib/pages/private_chat_list.dart
+++ snarf/lib/pages/private_chat_list.dart
@@ -1,40 +1,139 @@
-import 'package:flutter/material.dart';
-import 'package:snarf/pages/private_chat_page.dart';
+import 'dart:convert';
+import 'dart:developer';
 
-class PrivateChatListPage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:signalr_netcore/signalr_client.dart';
+import 'package:snarf/utils/api_constants.dart';
+import 'private_chat_page.dart';
+
+class PrivateChatListPage extends StatefulWidget {
   const PrivateChatListPage({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Mock para representar os chats privados
-    final List<String> privateChats = [
-      'Usuário 1',
-      'Usuário 2',
-      'Usuário 3',
-    ];
+  _PrivateChatListPageState createState() => _PrivateChatListPageState();
+}
 
+class _PrivateChatListPageState extends State<PrivateChatListPage> {
+  late HubConnection _hubConnection;
+  List<Map<String, dynamic>> _recentChats = [];
+  final FlutterSecureStorage _storage = FlutterSecureStorage();
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeSignalRConnection();
+  }
+
+  Future<String> getAccessToken() async {
+    return await _storage.read(key: 'token') ?? '';
+  }
+
+  Future<void> _initializeSignalRConnection() async {
+    // Configura o HubConnection
+    _hubConnection = HubConnectionBuilder()
+        .withUrl(
+            '${ApiConstants.baseUrl.replaceAll('/api', '')}/PrivateChatHub',
+            options: HttpConnectionOptions(
+                accessTokenFactory: () async => await getAccessToken()))
+        .withAutomaticReconnect()
+        .build();
+
+    // Registrar o callback para "ReceiveRecentChats"
+    _hubConnection.on("ReceiveRecentChats", _handleRecentChats);
+    _hubConnection.on("ReceivePrivateMessage", _receiveNewMessages);
+
+    // Iniciar conexão com o SignalR
+    await _hubConnection.start();
+
+    // Solicitar conversas recentes
+
+    _hubConnection.invoke("GetRecentChats");
+
+    setState(() {
+      _isLoading = false;
+    });
+  }
+
+  void _receiveNewMessages(List<Object?>? data) {
+    log('chamando sincronização');
+    _hubConnection.invoke("GetRecentChats");
+  }
+
+  void _handleRecentChats(List<Object?>? data) {
+    if (data != null && data.isNotEmpty) {
+      // Decodificar o JSON recebido (como string) para uma lista de mapas
+      final jsonString = data.first as String;
+      try {
+        final parsedData = jsonDecode(jsonString) as List<dynamic>;
+        setState(() {
+          _recentChats = parsedData.map((item) {
+            if (item is Map<String, dynamic>) {
+              return item;
+            } else if (item is Map) {
+              return Map<String, dynamic>.from(item); // Converte se necessário
+            } else {
+              throw Exception("Item inesperado no JSON: $item");
+            }
+          }).toList();
+        });
+      } catch (e) {
+        debugPrint('Erro ao processar JSON recebido: $e');
+      }
+    }
+  }
+
+  @override
+  void dispose() {
+    _hubConnection.stop();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Chats Privados'),
       ),
-      body: ListView.builder(
-        itemCount: privateChats.length,
-        itemBuilder: (context, index) {
-          final userId = privateChats[index];
-          return ListTile(
-            title: Text(userId),
-            onTap: () {
-              // Abrir a página de chat privado
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PrivateChatPage(userId: userId),
+      body: _isLoading
+          ? const Center(child: CircularProgressIndicator())
+          : _recentChats.isEmpty
+              ? const Center(
+                  child: Text(
+                    'Nenhuma conversa encontrada.',
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                )
+              : ListView.builder(
+                  itemCount: _recentChats.length,
+                  itemBuilder: (context, index) {
+                    final chat = _recentChats[index];
+                    return ListTile(
+                      title: Text(chat['UserName']),
+                      subtitle: Text(chat['LastMessage']),
+                      trailing: Text(
+                        _formatDate(chat['LastMessageDate']),
+                        style:
+                            const TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                PrivateChatPage(userId: chat['UserId']),
+                          ),
+                        );
+                      },
+                    );
+                  },
                 ),
-              );
-            },
-          );
-        },
-      ),
     );
+  }
+
+  String _formatDate(String dateString) {
+    final date = DateTime.parse(dateString);
+    return '${date.hour.toString().padLeft(2, '0')}:${date.minute.toString().padLeft(2, '0')}, ${date.day.toString().padLeft(2, '0')}/${date.month.toString().padLeft(2, '0')}/${date.year}';
   }
 }
