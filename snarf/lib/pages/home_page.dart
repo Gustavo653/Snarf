96fadec7e778f4afc5a7f729b6b8f9a5da3ba40d
MathFilipi <matfys2@gmail.com>
2025-04-09 21:25:42 -0300

    Implementacao de notificacao de chat  privado

--- snarf/lib/pages/home_page.dart
+++ snarf/lib/pages/home_page.dart
@@ -841,14 +841,49 @@ class _HomePageState extends State<HomePage> {
         showUnselectedLabels: false,
         onTap: (index) async {
           if (index == 0) {
+            configProvider.ClearNotification();
             _openPrivateChat(context);
           } else if (index == 1) {
             _openPublicChat(context);
           }
         },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.chat), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.group), label: ''),
+        items: [
+          // BottomNavigationBarItem(icon: Icon(Icons.chat), label: ''),
+          BottomNavigationBarItem(
+            icon: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                const Icon(Icons.chat),
+                if (configProvider.countNotificationMessage > 0)
+                  Positioned(
+                    right: -4,
+                    top: -6,
+                    child: Container(
+                      padding: const EdgeInsets.all(2),
+                      decoration: BoxDecoration(
+                        color: Colors.red,
+                        shape: BoxShape.circle,
+                      ),
+                      constraints: const BoxConstraints(
+                        minWidth: 16,
+                        minHeight: 16,
+                      ),
+                      child: Text(
+                        configProvider.countNotificationMessage.toString(),
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 10,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+            label: '',
+          ),
+          const BottomNavigationBarItem(icon: Icon(Icons.group), label: ''),
         ],
       ),
     );
